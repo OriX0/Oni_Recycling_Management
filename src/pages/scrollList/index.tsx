15034ef21e16282089191f6ba23cb9301da6f375
OriX <1098888910@qq.com>
2021-07-30 14:37:04 +0800

    chore:use mockjs  instead fakerjs

--- src/pages/scrollList/index.tsx
+++ src/pages/scrollList/index.tsx
@@ -5,14 +5,14 @@
  */
 import { VList } from './VList';
 import { WrapperContainer, SpanBottom, SpanTop } from './style';
-import faker from 'faker';
+import Mock from 'mockjs';
 
 const data: any = [];
 const dataLength = 10000;
 for (let id = 0; id < dataLength; ++id) {
   data.push({
     id,
-    value: faker.lorem.sentences(),
+    value: Mock.mock('@cparagraph(3,10)'),
   });
 }
 
