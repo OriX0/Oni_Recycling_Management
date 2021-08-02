4f53c6e655dad0757d65f37e21097d157f12e6f8
OriX <1098888910@qq.com>
2021-08-02 19:26:10 +0800

    chore:增加判断 防止重复初始化数据

--- src/utils/projectStorage.ts
+++ src/utils/projectStorage.ts
@@ -4,7 +4,7 @@
  * @LastEditors: OriX
  */
 import CustomerStorage from './customerStorage';
-import { RecycleRowData, RecycleStatus } from '@/types/recycleList';
+import { RecycleRowData } from '@/types/recycleList';
 import Mock from 'mockjs';
 import { generateMockDataArr } from '.';
 
@@ -15,30 +15,36 @@ projectStorage.bootStrap({ mode: 'local', timeout: 60 * 60 * 24 * 30 });
 const generateCount = 10;
 function generateRecycleData(indexG: number): RecycleRowData {
   const tempStatus = Mock.mock('@integer(0,5)');
-  if (tempStatus < 2) {
-    return {
-      studentId: Mock.mock('@integer(10000,30000)'),
-      status: tempStatus,
-      contactsName: Mock.mock('@cname'),
-      contactsAddress: '',
-      contactsPhone: '',
-      contactsTime: '',
-      indexG,
-    };
-  }
-  return {
+  const tempInfoObj = {
     studentId: Mock.mock('@integer(10000,30000)'),
     status: tempStatus,
     contactsName: Mock.mock('@cname'),
+    indexG,
+  };
+  if (tempStatus < 2) {
+    return {
+      ...tempInfoObj,
+      contactsAddress: '',
+      contactsPhone: '',
+      contactsTime: '',
+    };
+  }
+  return {
+    ...tempInfoObj,
     contactsAddress: Mock.mock('@county(true)'),
     contactsPhone: Mock.mock(
       /^(?:(?:\+|00)86)?1(?:(?:3[\d])|(?:4[5-79])|(?:5[0-35-9])|(?:6[5-7])|(?:7[0-8])|(?:8[\d])|(?:9[189]))\d{8}$/,
     ),
     contactsTime: Mock.mock('@date("MM-dd")@time("A HH:mm")'),
-    indexG,
   };
 }
-const mockRecycleData = generateMockDataArr(generateCount, generateRecycleData);
-projectStorage.setItem('recycleList', mockRecycleData);
+// 如果没有recycleList数据 则进行数据生成
+if (!projectStorage.getItem('recycleList')) {
+  const mockRecycleData = generateMockDataArr(
+    generateCount,
+    generateRecycleData,
+  );
+  projectStorage.setItem('recycleList', mockRecycleData);
+}
 
 export default projectStorage;
